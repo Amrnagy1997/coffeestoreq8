ba2c130a5a0f998f7091c7ed21a0e4b7613c02b1
Amrnagy1997 <140352195+Amrnagy1997@users.noreply.github.com>
2026-06-17 17:21:10 +0300

    Fix prisma generate issue for Hostinger deployment and update DB connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,12 @@
 import { PrismaClient } from "../generated/prisma";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL || "file:./dev.db",
-});
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
+    url: process.env.DATABASE_URL,
     log: ["query"],
-  });
-
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
